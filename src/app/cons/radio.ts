d9b6ca22dc5cc76636f099069f0f1a92e1d2a5d2
Luis Alejandro Meza <luismeza15@gmail.com>
2026-04-09 12:27:23 +0200

    crear constante de  radio

---
+++ src/app/cons/radio.ts
@@ -0,0 +1,24 @@
+type Radios = {
+    name: string,
+    url: string,
+    img?: string,
+}
+export const radiosData: Radios[] = [
+    {
+        name: "Blonded Radio",
+        url: "https://5fkef9udgm.ufs.sh/f/gkQPQrvJfr6GdKeuqCyWMFjTUrZw1OICfBVbPlK6q3vXgsm2"
+    },
+    {
+        name: "World Wide FM",
+        url: "https://5fkef9udgm.ufs.sh/f/8n7sXoLh2m3aGdKeuqCyWMFjTUrZw1OICfBVbPlK6q3vXgsm2",
+    },
+    {
+        name: "Non Stop Pop FM",
+        url: "https://5fkef9udgm.ufs.sh/f/8n7sXoLh2m3aGdKeuqCyWMFjTUrZw1OICfBVbPlK6q3vXgsm2",
+    },
+    {
+        name: "FlyLo FM",
+        url: "https://5fkef9udgm.ufs.sh/f/gkQPQrvJfr6GGWq6U5NTurIYbOT76l0f92wBWZzAFh4UtqLQ",
+
+    }
+];
