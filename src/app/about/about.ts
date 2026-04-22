26d8269cdd27a8fa3d736031e9fed3a1b41db95f
Luis Alejandro Meza <luismeza15@gmail.com>
2026-04-22 13:16:07 +0200

    header component

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Countdown } from '../countdown/countdown';
-
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-about',
-  imports: [Countdown],
+  imports: [Countdown, Header],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Player } from './player/player';
 import { About } from './about/about';
 import { Countdown } from './countdown/countdown';
+import { Header } from './header/header';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'player' },
   { path: 'player', component: Player },
   { path: 'about', component: About },
   { path: 'countdown', component: Countdown },
+  { path: 'header', component: Header },
 
 ];
